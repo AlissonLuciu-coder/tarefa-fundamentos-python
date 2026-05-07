fbcebfb3d24efda705bb770dad69e6a5ccf13049
AlissonLuciu-coder <alisson.luciu@gmail.com>
2026-05-06 21:16:25 -0300

    Ta fluindo

--- ex1.py
+++ ex1.py
@@ -1,3 +1,4 @@
+import subprocess
 lista_de_produtos = []
 lista_de_precos = []
 
@@ -13,6 +14,19 @@ while True:
     if continuar == 'sair':
         break
 
+
+media_dos_precos = sum(lista_de_precos) / len(lista_de_precos)
+
+caro = max(lista_de_precos)
+posicao_caro = lista_de_precos.index(caro)
+nome_caro = lista_de_produtos[posicao_caro]
+
+
+print(f'A média dos preços é: {media_dos_precos:.2f}')
+
+for i in range(len(lista_de_produtos)):
+    print(f'Produto: {lista_de_produtos[i]}, Preço: R$ {lista_de_precos[i]:.2f}')
+
 print(lista_de_produtos)
 print(lista_de_precos)
-    
+print(f'O produto {nome_caro} custa R${caro:.2f}') 
